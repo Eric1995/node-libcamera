bfcdf6e5683255cb35f532d762de133f5f18cd82
Eric1995 <ericding95@outlook.com>
2024-02-07 11:21:50 +0800

    delete unused files

--- camera/cpp/core/logging.hpp
+++
@@ -1,9 +0,0 @@
-// #include "core/rpicam_app.hpp"
-
-#define LOG(level, text)                                                                                               \
-	do                                                                                                                 \
-	{                                                                                                                  \
-		if (1 >= level)                                                                     \
-			std::cerr << text << std::endl;                                                                            \
-	} while (0)
-#define LOG_ERROR(text) std::cerr << text << std::endl
