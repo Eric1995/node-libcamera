6b6e1fe7a7a4b81af9a64a9eb8ed8844405f7441
Eric1995 <ericding95@outlook.com>
2025-09-10 18:54:23 +0800

    update camera logic

--- src/Camera.ts
+++ src/Camera.ts
@@ -1,40 +1,56 @@
-import Stream from "./Stream";
-import type { RawCamera } from "./types";
+import Image from './Image';
+import Stream from './Stream';
+import type { PixelFormat, RawCamera, StreamRole } from './types';
 
 class Camera {
-    private camera: RawCamera;
-    constructor(_camera: RawCamera) {
-        this.camera = _camera;
-    }
+  private camera: RawCamera;
+  constructor(_camera: RawCamera) {
+    console.log('create camera');
+    this.camera = _camera;
+  }
 
-    public getInfo() {
-        return this.camera.getInfo();
-    }
+  public getInfo() {
+    return this.camera.getInfo();
+  }
 
-    public config(option: Parameters<RawCamera['config']>[0]) {
-        return this.camera.config(option);
-    }
+  public config(option: Parameters<RawCamera['config']>[0]) {
+    return this.camera.config(option);
+  }
 
-    public createStreams(option: Parameters<RawCamera['createStreams']>[0]) {
-        const streams =  this.camera.createStreams(option);
-        return streams.map(s => new Stream(s));
-    }
+  public createStreams(
+    option: {
+      width: number;
+      height: number;
+      role?: StreamRole;
+      pixel_format?: PixelFormat;
+      onImageData?: (err: unknown, ok: boolean, image: Image) => void;
+    }[],
+  ) {
+    const streams = this.camera.createStreams(option);
+    // immediately overwrite onImageData
+    return streams.map((s) => {
+      const st = new Stream(s);
+      st.config({
+        onImageData: option[s.streamIndex].onImageData,
+      });
+    });
+  }
 
-    public start() {
-        this.camera.start();
-    }
+  public start() {
+    this.camera.start();
+  }
 
-    public stop() {
-        this.camera.stop();
-    }
+  public stop() {
+    this.camera.stop();
+  }
 
-    public release() {
-        this.camera.release();
-    }
+  public release() {
+    this.camera.release();
+  }
 
-    public sendRequest() {
-        return this.camera.sendRequest();
-    }
+  public sendRequest() {
+    return this.camera.sendRequest();
+  }
 }
 
 export default Camera;
